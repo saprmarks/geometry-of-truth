663ee33cb2a92ab0776e5b72fbc040bb0d82faad
Sam Marks <saprmarks@gmail.com>
2024-01-22 17:25:39 -0500

    Creating xor datasets

--- datasets/xor.py
+++ datasets/xor.py
@@ -2,19 +2,20 @@ import pandas as pd
 
 likely = pd.read_csv('datasets/likely.csv')
 
-n_statements = 1000
 
 statements = []
+labels = []
 
 for _, row in likely.iterrows():
-    if row['label'] == 0:
-        continue
     statements.append(row['statement'])
-    if len(statements) == n_statements:
-        break
+    labels.append(row['label'] == 1)
 
-trues = [1] * (n_statements // 2) + [0] * (n_statements // 2)
-bananas = [1] * (n_statements // 4) + [0] * (n_statements // 4) + [1] * (n_statements // 4) + [0] * (n_statements // 4)
+n = (len(statements) // 4) * 4
+statements = statements[:n]
+labels = labels[:n]
+
+trues = [True] * (n // 2) + [False] * (n // 2)
+bananas = [True] * (n // 4) + [False] * (n // 4) + [True] * (n // 4) + [False] * (n // 4)
 
 for idx, (statement, true, banana) in enumerate(zip(statements, trues, bananas)):
     statements[idx] = statement + (' true' if true else ' false') + (' banana' if banana else ' shed')
@@ -23,7 +24,11 @@ df = pd.DataFrame({
     'statement': statements,
     'true': trues,
     'banana': bananas,
-    'xor': [true ^ banana for true, banana in zip(trues, bananas)]
+    'label': labels,
+    'has_true xor has_banana': [true ^ banana for true, banana in zip(trues, bananas)],
+    'has_true xor has label' : [true ^ label for true, label in zip(trues, labels)],
+    'has_banana xor has label' : [banana ^ label for banana, label in zip(bananas, labels)],
+    'has_true xor has_banana xor has_label' : [true ^ banana ^ label for true, banana, label in zip(trues, bananas, labels)],
 })
 
-df.to_csv('datasets/xor.csv', index=False)
+df.to_csv('datasets/xor_new.csv', index=False)
